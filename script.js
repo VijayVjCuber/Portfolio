e702fabde2e100008099ae9ab0df40d3f09d50d2
unknown <vijaygowtham275@gmail.com>
2020-06-10 00:12:01 +0530

    Responsive navbar and clippath polygon

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 const darkbutton=document.getElementById("dark");
 const lightbutton=document.getElementById("light");
 const body = document.body;
+const menu = document.getElementsByClassName("hamburger")[0];
+const navbar = document.getElementsByClassName("main-nav")[0];
 
+// THEMES
 darkbutton.onclick = () => {
     body.classList.replace('light', 'dark');
 }
@@ -9,3 +12,14 @@ darkbutton.onclick = () => {
 lightbutton.onclick = () => {
     body.classList.replace('dark', 'light');
 }
+
+
+// CLICK EVENT FOR HAMBURGER MENU
+menu.addEventListener('click', () => {
+    if(navbar.style.display === "flex"){
+        navbar.style.display = "none";
+    }
+    else{
+        navbar.style.display = "flex";
+    }
+})
